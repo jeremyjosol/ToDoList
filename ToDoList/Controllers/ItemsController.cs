017a4f2fc083782917f8d961064aaccb33dee986
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-25 14:01:53 -0700

    Add new files to consolidate behaviors

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+    [HttpGet("items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return RedirectToAction("Index");
+    }
+  }
+}
