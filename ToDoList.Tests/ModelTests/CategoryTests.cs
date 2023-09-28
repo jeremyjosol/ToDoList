ea50474af28301f2f321601d586c65762b9b383c
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-27 21:05:40 -0700

    Create new tests + pass tests

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -6,8 +6,33 @@ using System;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class CategoryTests
+  public class CategoryTests : IDisposable
   {
-
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
+    [TestMethod]
+    public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+    {
+      Category newCategory = new Category("test category");
+      Assert.AreEqual(typeof(Category), newCategory.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+      string result = newCategory.Name;
+      Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+      int result = newCategory.Id;
+      Assert.AreEqual(1, result);
+    }
   }
 }
