4b8ea971d44fc2ca0326c669f7e8dbccb79fd06f
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-01 13:46:53 -0700

    Update .gitignore- + .DS_Store + appsettings.json

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System;
+using System.Collections.ObjectModel;
 
 namespace ToDoList.Tests
 {
@@ -33,6 +34,39 @@ namespace ToDoList.Tests
       Category newCategory = new Category(name);
       int result = newCategory.Id;
       Assert.AreEqual(1, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      List<Category> newList = new List<Category> { newCategory1, newCategory2};
+      List<Category> result = Category.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void Find_ReturnsCorrectCategory_Category()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      Category result = Category.Find(2);
+      Assert.AreEqual(newCategory2, result);
+    }
+    [TestMethod]
+    public void AddItem_AssociatesItemWithCategory_ItemList()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      List<Item> newList = new List<Item> { newItem };
+      string name = "Work";
+      Category newCategory = new Category(name);
+      newCategory.AddItem(newItem);
+      List<Item> result = newCategory.Items;
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
