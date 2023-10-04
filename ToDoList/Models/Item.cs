4a84e8c19f2c048a88334c47efaca9e9a4a1ba99
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-03 22:34:35 -0700

    Set up Item + Category relationship

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -7,5 +7,7 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int ItemId { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
   }
 }
