824e463ea4c4cd026c50a20fdf66bb84d6c37a9f
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-08 15:25:15 -0700

    Update Details action to fetch JoinEntities

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -42,6 +42,8 @@ namespace ToDoList.Controllers
     {
       Item thisItem = _db.Items
                          .Include(item => item.Category)
+                         .Include(item => item.JoinEntities)
+                         .ThenInclude(join => join.Tag)
                          .FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
