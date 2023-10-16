01fb40f31b615c3995e26a5062e198a442ca118d
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-16 10:08:58 -0700

    Add Create action + corresponding View

--- ToDoList/Controllers/RoleController.cs
+++ ToDoList/Controllers/RoleController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
- 
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
 namespace Identity.Controllers
 {
     public class RoleController : Controller
@@ -17,6 +19,21 @@ namespace Identity.Controllers
         {
             foreach (IdentityError error in result.Errors)
                 ModelState.AddModelError("", error.Description);
+        }
+        public IActionResult Create() => View();
+ 
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            return View(name);
         }
     }
 }
